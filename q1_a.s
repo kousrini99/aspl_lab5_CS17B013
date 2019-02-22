a2148c6cb0105e54e8022bea70a2385551029633
kousrini99 <32215071+kousrini99@users.noreply.github.com>
2019-02-22 17:20:44 +0530

    Add files via upload

---
+++ q1_a.s
@@ -0,0 +1,17 @@
+main:
+	addi $sp,$sp,-16
+	sw   $s0,12($sp)
+	sw   $s1,8($sp)
+	sw   $s2,4($sp)
+	sw   $s3,0($sp)
+	li   $s0,3
+	li   $s1,3
+	mult $s0,$s1
+	mfhi $s2
+	mflo $s3
+	lw   $s3,0($sp)
+	lw   $s2,4($sp)
+	lw   $s1,8($sp)
+	lw   $s0,12($sp)
+	addi $sp,$sp,16
+	j    $31
